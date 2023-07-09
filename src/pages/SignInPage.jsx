cac46614c0a32e6816a334c1d7a2e7aa947d11b2
GabrielBragaa <gbragamoura@hotmail.com>
2023-07-09 17:29:58 -0300

    envio dos dados de login

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import { Link } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
 import { useEffect, useState } from "react"
+import axios from 'axios';
 
 export default function SignInPage() {
   const [email, setEmail] = useState('');
@@ -10,12 +11,27 @@ export default function SignInPage() {
     email: '', 
     password: ''};
 
+  function signIn(e) {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    user = {email, password};
+
+    axios.post(`${import.meta.env.VITE_API_URL}/`, user)
+    .then(console.log('OK'))
+    .catch(err => alert(err.response.data))
+  }
+
   return (
     <SingInContainer>
-      <form>
-        <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autoComplete="new-password" />
+      <form onSubmit={signIn}>
+        <MyWalletLogo/>
+        <input placeholder="E-mail" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input placeholder="Senha" type="password" autoComplete="new-password" value={password} onChange={e => setPassword(e.target.value)} />
         <button>Entrar</button>
       </form>
 
