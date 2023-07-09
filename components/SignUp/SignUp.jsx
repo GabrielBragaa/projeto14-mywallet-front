78aeb52820f0b2c687df9205a64638fad075d2d9
GabrielBragaa <gbragamoura@hotmail.com>
2023-07-09 13:50:25 -0300

    layout disponibilizado

---
+++ components/SignUp/SignUp.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export default function SignUp() {
+    return (
+        <SCBody>
+            <p>Oi</p>
+        </SCBody>
+    )
+}
+
+const SCBody = styled.body `
+    width: 100%;
+    background-color: purple;
+`
+
